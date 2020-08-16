00752ab728ce55dff27818c2ae8e67ca52f4f03f
Mike <37061388+mwonder1@users.noreply.github.com>
2020-08-15 23:51:04 -0400

    RecyclerView functionality.

--- app/src/main/java/com/example/android/financeapp01/MainActivity.kt
+++ app/src/main/java/com/example/android/financeapp01/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.android.financeapp01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        val exampleList = generateDummyList(500)
+
+        rvStocks.adapter = StockAdapter(exampleList)
+        rvStocks.layoutManager = LinearLayoutManager(this)
+        rvStocks.setHasFixedSize(true)
+
+    }
+
+    private fun generateDummyList(size: Int): List<StockItem> {
+        val list = ArrayList<StockItem>()
+        for (i in 0 until size) {
+            val drawable = when (i % 2) {
+                0 -> R.drawable.ic_arrow_up
+                else -> R.drawable.ic_arrow_down
+
+            }
+            val item = StockItem(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
     }
 }
--- app/src/main/java/com/example/android/financeapp01/StockAdapter.kt
+++ app/src/main/java/com/example/android/financeapp01/StockAdapter.kt
@@ -8,25 +8,28 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.stock_item.view.*
 
-class StockAdapter : RecyclerView.Adapter<StockAdapter.StockViewHolder>() {
+class StockAdapter(private val stockList: List<StockItem>) :
+    RecyclerView.Adapter<StockAdapter.StockViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.stock_item, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.stock_item, parent, false)
 
         return StockViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
+        val currentItem = stockList[position]
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
     }
 
-    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = stockList.size
 
     class StockViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.image_view
         val textView1: TextView = itemView.text_view_1
-        val textView: TextView = itemView.text_view_2
+        val textView2: TextView = itemView.text_view_2
     }
 }
